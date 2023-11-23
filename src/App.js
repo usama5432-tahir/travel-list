72fa735149102dbb6783c8e7d7e57bf21eb6e148
usama5432-tahir <114473564+usama5432-tahir@users.noreply.github.com>
2023-11-23 19:33:13 +0500

    update.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ function App() {
   const [items, setItems] = useState([]);
 
   function handleAddItems(item) {
-    setItems((items) => [...items, item]);
+    setItems((prevItems) => [...prevItems, item]);
   }
 
   function handleDeleteItems(id) {
-    setItems((items) => items.filter((item) => item.id !== id));
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   }
+
   function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) => item.id === id ? {...item, packed: !item.packed} : item)
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
     );
   }
 
   return (
     <div className="app">
       <Logo />
-      <Form onAddItems={handleAddItems} onDeleteItems={handleDeleteItems} />
-      <PackingList items={items} onDeleteItems={handleDeleteItems} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -34,7 +41,7 @@ function Logo() {
   );
 }
 
-function Form({ onAddItems, onDeleteItems , onToggleItem}) {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -43,8 +50,9 @@ function Form({ onAddItems, onDeleteItems , onToggleItem}) {
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
     onAddItems(newItem);
-
-    console.log(newItem);
+    // Reset the form after adding an item
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
@@ -61,6 +69,7 @@ function Form({ onAddItems, onDeleteItems , onToggleItem}) {
       <input
         type="text"
         placeholder="item..."
+        value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
       <button>Add</button>
@@ -73,7 +82,12 @@ function PackingList({ items, onDeleteItems, onToggleItem }) {
     <div className="">
       <ul className="list">
         {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItems={onDeleteItems} onToggleItem={onToggleItem}/>
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItems={onDeleteItems}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
@@ -84,20 +98,32 @@ function Item({ item, onDeleteItems, onToggleItem }) {
   return (
     <>
       <li>
-        <input type="checkbox" checked={item.packed} onChange={()=>onToggleItem(item.id)} />
-        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        <input
+          type="checkbox"
+          checked={item.packed}
+          onChange={() => onToggleItem(item.id)}
+        />
+        <span
+          style={item.packed ? { textDecoration: "line-through" } : {}}
+        >
           {item.quantity} {item.description}
         </span>
-        <button onClick={() => onDeleteItems(item)}>❌</button>
+        <button onClick={() => onDeleteItems(item.id)}>❌</button>
       </li>
     </>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) return (
+   <p className="stats">No items added yet</p>);
+  const numItems = items.length;
+  const numPackedItems = items.filter((item) => item.packed).length;
+  const percentage = (numPackedItems / numItems)*100;
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X(X%)</em>
+      <em>{ percentage ==100 ? "You are ready to go!" : ` You have ${numItems} items on your list, and you already packed ${numPackedItems}(${percentage}%)`}
+       </em>
     </footer>
   );
 }
